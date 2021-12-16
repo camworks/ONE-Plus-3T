31796c30acc2190163630daf4da6a9c664813bd2
Comma Device <device@comma.ai>
2021-12-16 20:34:35 +0900

    tune scc

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -25,7 +25,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
 
     gas_max_bp = [0., 10, 20., 50., 70., 100, 130.]
-    gas_max_v = [CarControllerParams.ACCEL_MAX, 1.7, .8, 0.7, 0.45, 0.25, 0.1]
+    gas_max_v = [CarControllerParams.ACCEL_MAX, 1.4, .78, 0.65, 0.45, 0.25, 0.1]
 
     return CarControllerParams.ACCEL_MIN, interp(v_current_kph, gas_max_bp, gas_max_v)
 
