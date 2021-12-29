012d95133bc7c698e2d855bc5d306bf6cb060cf2
boxkon <70835190+boxkon@users.noreply.github.com>
2021-12-30 07:25:02 +0900

    Increase Max_Steer and Max_Accel

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -1,4 +1,4 @@
-const int HYUNDAI_MAX_STEER = 409;             // like stock
+const int HYUNDAI_MAX_STEER = 500;             // like stock
 const int HYUNDAI_MAX_RT_DELTA = 112;          // max delta torque allowed for real time checks
 const uint32_t HYUNDAI_RT_INTERVAL = 250000;   // 250ms between real time checks
 const int HYUNDAI_MAX_RATE_UP = 5;
@@ -7,7 +7,7 @@ const int HYUNDAI_DRIVER_TORQUE_ALLOWANCE = 50;
 const int HYUNDAI_DRIVER_TORQUE_FACTOR = 2;
 const int HYUNDAI_STANDSTILL_THRSLD = 30;  // ~1kph
 
-const int HYUNDAI_MAX_ACCEL = 200;  // 1/100 m/s2
+const int HYUNDAI_MAX_ACCEL = 350;  // 1/100 m/s2
 const int HYUNDAI_MIN_ACCEL = -350; // 1/100 m/s2
 
 const CanMsg HYUNDAI_TX_MSGS[] = {
